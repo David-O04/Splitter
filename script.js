d57940755a38d23a6edf66ac3086f3bbbc818cc2
David-O04 <davidoberiko@gmail.com>
2021-11-02 09:42:52 +0000

    added shift key function

--- script.js
+++ script.js
@@ -21,7 +21,26 @@ function addQuarter(intoDiv){
     intoDiv.appendChild(q)
 
     
+    function split4(e){
 
+        if(e.shiftKey){
+            //shift is pressed - lets all the children of my parent (that includes me!)
+            this.parentNode.addEventListener("click",split4)
+            this.parentNode.innerHTML=''  //or delete the children a nicer way
+        }
+        else{
+            //Split a div into 4 by adding 4 quarters
+    
+           // this.removeEventListener("click",split4)
+            addQuarter(this)
+            addQuarter(this)
+            addQuarter(this)
+            addQuarter(this)
+        }
+    
+        // Event 'capturing and bubbling' 
+        e.stopPropagation() //stop the event from firing again on all the ancestor divs
+    }
 
 
 }
